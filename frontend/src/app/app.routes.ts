780b5b3579c1a3496ffc1b42711f6ca1d1ad11b8
mohidreekh <mohammad.idreekh00@gmail.com>
2025-07-24 20:20:30 +0300

    add lesson to course and add video to lesson

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,15 +5,14 @@ import { MainScreenComponent } from './moduls/main-screen/main-screen.component'
 import { DonationScreenComponent } from './moduls/donation-screen/donation-screen.component';
 import { CoursesComponent } from './moduls/courses/courses.component';
 import { QuestionComponent } from './moduls/question/question.component';
-import {AddVideoToLessonComponent} from './add-video-to-lesson/add-video-to-lesson.component';
-
+import { AddVideoToLessonComponent } from './moduls/add-video-to-lesson/add-video-to-lesson.component';
 
 export const routes: Routes = [
-    { path: '', component: MainScreenComponent },
-    { path: 'lessons' , component : LessonsListComponent},
-    { path: 'video/:videoId', component: VideoPlayerComponent },
-    { path: 'donation' , component: DonationScreenComponent},
-    { path: 'course', component : CoursesComponent },
-    { path: 'question', component: QuestionComponent },  // it should be deleted after the configuration
-    { path: 'addVideo', component: AddVideoToLessonComponent},
+  { path: '', component: MainScreenComponent },
+  { path: 'lessons', component: LessonsListComponent },
+  { path: 'video/:videoId', component: VideoPlayerComponent },
+  { path: 'donation', component: DonationScreenComponent },
+  { path: 'course', component: CoursesComponent },
+  { path: 'question', component: QuestionComponent }, // it should be deleted after the configuration
+  { path: 'addVideo', component: AddVideoToLessonComponent },
 ];
--- frontend/src/app/moduls/add-video-to-lesson/add-video-to-lesson.component.ts
+++ frontend/src/app/moduls/add-video-to-lesson/add-video-to-lesson.component.ts
@@ -1,37 +1,32 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import {ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
-import { LessonsService } from '../services/lessons.service';
-import { Video } from '../models/video.model';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { LessonsService } from '../../services/lessons.service';
+import { Video } from '../../models/video.model';
 
 @Component({
   selector: 'app-add-video-to-lesson',
   imports: [ReactiveFormsModule],
   templateUrl: './add-video-to-lesson.component.html',
-  styleUrl: './add-video-to-lesson.component.css'
+  styleUrl: './add-video-to-lesson.component.css',
 })
-export class AddVideoToLessonComponent implements OnInit
-{
-
-  videoForm !: FormGroup;
+export class AddVideoToLessonComponent implements OnInit {
+  videoForm!: FormGroup;
   @Input() lessonId!: number;
 
   @Output() onBack = new EventEmitter<boolean>();
-    
+
   goBack() {
-   this.onBack.emit(true);
+    this.onBack.emit(true);
   }
 
-  constructor(private lessonsService:LessonsService){}
+  constructor(private lessonsService: LessonsService) {}
 
-
-  videoToAdd !: Video
-
+  videoToAdd!: Video;
 
   ngOnInit(): void {
-
     this.videoForm = new FormGroup({
-      videoTitle : new FormControl(""),
-      videoUrl : new FormControl("")
+      videoTitle: new FormControl(''),
+      videoUrl: new FormControl(''),
     });
 
     console.log(this.videoForm.value);
@@ -40,10 +35,10 @@ export class AddVideoToLessonComponent implements OnInit
   onSubmit() {
     if (this.videoForm.valid) {
       this.videoToAdd = {
-        id:0,
+        id: 0,
         title: this.videoForm.value.videoTitle,
         video_url: this.videoForm.value.videoUrl,
-        lesson_id: this.lessonId
+        lesson_id: this.lessonId,
       };
 
       this.lessonsService.addVideoToLesson(this.videoToAdd).subscribe({
@@ -54,13 +49,10 @@ export class AddVideoToLessonComponent implements OnInit
         error: (error) => {
           console.error('Error:', error);
           alert('Added Fiald');
-        }
+        },
       });
-    } 
-    else {
+    } else {
       alert('Invalid Form');
     }
-
   }
-
 }
