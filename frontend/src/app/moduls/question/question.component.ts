f6ee7d1f0b3661f9cbbd62ec3a0997b1a92e8be1
Omar Safarini <omarsafarini123344@gmail.com>
2025-07-24 19:45:30 +0300

    finishing all the tasks

--- frontend/src/app/moduls/question/question.component.ts
+++ frontend/src/app/moduls/question/question.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Question } from '../../models/question.model';
+import { QuestionService } from '../../services/question.service';
 
 @Component({
   selector: 'app-question',
@@ -10,27 +10,32 @@ import { Question } from '../../models/question.model';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent {
-  questionData: Question[] = [
-    { id: 1, lesson_id: 1, lesson: 'معلومات حول كوكو', question_body: 'من هو كوكو', answer_body: 'كوكو', is_true: false },
-    { id: 2, lesson_id: 1, lesson: 'معلومات حول كوكو', question_body: 'من هو كوكو', answer_body: 'محمد ابو كوكو', is_true: true },
-    { id: 3, lesson_id: 1, lesson: 'معلومات حول كوكو', question_body: 'من هو كوكو', answer_body: 'كوكو واوا', is_true: false },
-    { id: 4, lesson_id: 1, lesson: 'معلومات حول كوكو', question_body: 'من هو كوكو', answer_body:'كوكو ابن ابو علي', is_true: false },
-
-    { id: 5, lesson_id: 1, lesson: 'معلومات حول كوكو', question_body: 'اين يعيش كوكو', answer_body: 'في الغابة', is_true: true },
-    { id: 6, lesson_id: 1, lesson: 'معلومات حول كوكو', question_body: 'اين يعيش كوكو', answer_body: 'في المريخ', is_true: false },
-    { id: 7, lesson_id: 1, lesson: 'معلومات حول كوكو', question_body: 'اين يعيش كوكو', answer_body: 'في البحر', is_true: false },
-    { id: 8, lesson_id: 1, lesson: 'معلومات حول كوكو', question_body: 'اين يعيش كوكو', answer_body: 'في المدينة', is_true: false },
-  ];
-
+  questionData: any = {}; 
+  questions: any[] = []; 
+  questionsGrouped: any[][] = [];
+  lessonHeader: string = '';
   choices = ['A', 'B', 'C', 'D'];
   usersAnswer: { Question: string; Answer: string }[] = [];
-  questionsGrouped = this.groupQuestions();
-  resultMessage = ''; 
-  
-  groupQuestions() {
+  resultMessage = '';
+
+  constructor(private questionService: QuestionService) {
+    this.questionService.getQuestionsWithAnswers(1).subscribe(data => {
+      this.questionData = data;
+      this.lessonHeader = this.questionData.name;
+      this.questions = this.questionData.questions.flatMap((q: any) =>
+        q.answers.map((a: any) => ({
+          ...a,
+          question_body: q.question_body
+        }))
+      );
+      this.questionsGrouped = this.groupQuestions(this.questions);
+    });
+  }
+
+  groupQuestions(questions: any[]) {
     const grouped = [];
-    for (let i = 0; i < this.questionData.length; i += 4) {
-      grouped.push(this.questionData.slice(i, i + 4));
+    for (let i = 0; i < questions.length; i += 4) {
+      grouped.push(questions.slice(i, i + 4));
     }
     return grouped;
   }
@@ -40,37 +45,36 @@ export class QuestionComponent {
   }
 
   isAnswerSelected(question: string, answer: string): boolean {
-  return this.usersAnswer.some(q => q.Question === question && q.Answer === answer);
-}
+    return this.usersAnswer.some(q => q.Question === question && q.Answer === answer);
+  }
 
   popWantedQuestionFromUserAnswer(question: string) {
     this.usersAnswer = this.usersAnswer.filter(q => q.Question !== question);
   }
 
   onChoiceClick(Question: string, Answer: string) {
-    if(this.isAnswersSelectedBeforeForQuestion(Question))
+    if (this.isAnswersSelectedBeforeForQuestion(Question))
       this.popWantedQuestionFromUserAnswer(Question);
     this.usersAnswer.push({ Question, Answer });
-    console.log(this.usersAnswer); // delete when you are done with the testing
   }
-  
+
   onSubmitClick() {
     let counter = 0;
-    for (const group of this.questionsGrouped) {
-      const questionText = group[0].question_body;
-      const userAnswer  = this.usersAnswer.find(u => u.Question === questionText);
-      if (userAnswer) {
-        const correctAnswer = group.find(ans => ans.is_true);
-        if (correctAnswer && correctAnswer.answer_body === userAnswer.Answer) {
-          counter++;
-        }
-      }
-    }
-      this.resultMessage = `أجبت على ${counter}  من اصل ${this.questionsGrouped.length} إجابة صحيحة.`;
+    const correctMap = new Map<string, string>();
+    this.questionData.questions.forEach((q: any) => {
+      const correct = q.answers.find((a: any) => a.is_true);
+      console.log(correct)
+      if (correct) correctMap.set(q.question_body, correct.answer_body);
+    });
+    
+    this.usersAnswer.forEach(user => {
+      console.log(correctMap.get(user.Question));
+      if (correctMap.get(user.Question) === user.Answer) counter++;
+    });
 
+    this.resultMessage = `أجبت على ${counter} من أصل ${this.questionData.questions.length} إجابة صحيحة.`;
     const modal = new (window as any).bootstrap.Modal(document.getElementById('resultModal'));
     modal.show();
-
     this.usersAnswer = [];
   }
 }
